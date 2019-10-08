4c6dfbc87179287ac805730b3a3ae1959fbf67e9
wapiti08 <tzrzhuoran@163.com>
2019-10-08 17:25:13 +0100

    Update main.py
    modified to python3

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from incdbscanner import *
 import csv
 import re
+import sys
+sys.path.append('./')
 
-configPath = '/home/aeon/Proj/IncrementalDBSCAN/config'
-dataPath = '/home/aeon/Proj/IncrementalDBSCAN/AccidentData.csv'
+configPath = 'config'
+dataPath = 'AccidentData.csv'
 
 def main():
     [Data,eps,MinPts]= getData()
-    print len(Data)
+    print(len(Data))
     indbc= incdbscanner()
     indbc.incdbscan(Data, eps, MinPts)
 def getData():
     Data = []
 
-    with open(dataPath,'rb') as f:
+    with open(dataPath, 'r') as f:
         reader = csv.reader(f)
         for row in reader:
             Data.append([float(row[0]),float(row[1])])
@@ -22,7 +24,7 @@ def getData():
     
     [eps,MinPts] = parse(f.readline())
     
-    print eps,MinPts
+    print(eps,MinPts)
     
     return [Data,eps,MinPts]
 
